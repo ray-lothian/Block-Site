d617dd6e555bfa85383f727a9260df0391f51893
Ray Lothian <ray.lothian@yahoo.com>
2022-06-15 00:11:11 -0400

    version 0.4.4

---
+++ v3/data/blocked/rate.js
@@ -0,0 +1,30 @@
+chrome.storage.local.get({
+  'rate': true,
+  'crate': 0
+}, prefs => {
+  document.getElementById('rate').dataset.hide = prefs['rate'] === false || prefs.crate < 5 || Math.random() < 0.5;
+
+  if (prefs.crate < 5) {
+    prefs.crate += 1;
+    chrome.storage.local.set({crate: prefs.crate});
+  }
+});
+
+document.getElementById('rate').onclick = () => {
+  let url = 'https://chrome.google.com/webstore/detail/block-site/lebiggkccaodkkmjeimmbogdedcpnmfb/reviews/';
+  if (/Edg/.test(navigator.userAgent)) {
+    url = 'https://microsoftedge.microsoft.com/addons/detail/deiilejafkhhgekckholkdhfhopcnmeb';
+  }
+  else if (/Firefox/.test(navigator.userAgent)) {
+    url = 'https://addons.mozilla.org/firefox/addon/block-website/reviews/';
+  }
+  else if (/OPR/.test(navigator.userAgent)) {
+    url = 'https://addons.opera.com/extensions/details/block-site-2/';
+  }
+
+  chrome.storage.local.set({
+    'rate': false
+  }, () => chrome.tabs.create({
+    url
+  }));
+};
